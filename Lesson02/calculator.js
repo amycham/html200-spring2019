9799e4946fa717c2beea00cd1030d09e21baad97
amycham <amycham@gmail.com>
2019-04-15 14:38:54 -0700

    required JS assignements

---
+++ Lesson02/calculator.js
@@ -0,0 +1,11 @@
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+
+console.log(b / d);
+console.log(d / b);
+console.log(a - b * c);
+console.log((a - b) * c);
+console.log(d % b);
+console.log(b % d);
