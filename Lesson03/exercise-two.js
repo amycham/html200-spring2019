6fa282d89fae708e4a01e4c133c011238fdd48e2
amycham <amycham@gmail.com>
2019-04-19 12:33:51 -0700

    second js exercise done

---
+++ Lesson03/exercise-two.js
@@ -0,0 +1,9 @@
+//declare sentence w/five words and period
+let sentence = "This is my little sentence.";
+
+//create array from sentence
+let wordArray = sentence.split(' ');
+let wordThree = wordArray[2];
+
+//output third word
+console.log(wordThree);
